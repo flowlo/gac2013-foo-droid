854befbde1e5146171b54804382081420e95d6c0
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-08-14 17:23:50 +0100

    Get rid of build folder, add browsing and login

---
+++ freecycle/AndroidStudioProject/Freecycle/src/main/java/gac/freecycle/BrowsingActivity.java
@@ -0,0 +1,23 @@
+package gac.freecycle;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BrowsingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browsing);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.browsing, menu);
+        return true;
+    }
+    
+}
