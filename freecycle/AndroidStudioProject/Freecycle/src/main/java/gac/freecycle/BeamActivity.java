af1199f9d8b6fd69067f7914191becc326b8673c
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-08-15 18:02:48 +0100

    Fist try on Android Beam

---
+++ freecycle/AndroidStudioProject/Freecycle/src/main/java/gac/freecycle/BeamActivity.java
@@ -0,0 +1,10 @@
+package gac.freecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeamActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
